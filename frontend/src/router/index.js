eba4027f3dddc34e7fb167973ee03f5018e65109
Delissa <delissaburton@gmail.com>
2025-02-26 14:57:32 -0500

    Hardware_warmos complete

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'Home', component: () => import('@/views/Home.vue'), meta:{ transition: 'fade'} },  
   { path: '/', name: 'Control', component: () => import('@/views/Control.vue'), meta:{ transition: 'fade'} }, 
-  { path: '/', name: 'Analysis', component: () => import('@/views/Analsis.vue'), meta:{ transition: 'fade'} }, 
+  { path: '/', name: 'Analysis', component: () => import('@/views/Analysis.vue'), meta:{ transition: 'fade'} }, 
   { path: '/', name: 'Dashboard', component: () => import('@/views/Dashboard.vue'), meta:{ transition: 'fade'} }, 
 ]
 
